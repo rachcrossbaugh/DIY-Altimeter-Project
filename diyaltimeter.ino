088e3cb5677f255abcb0a83b9ce58b027cd32b9a
Christian <39501368+christiancrossbaugh@users.noreply.github.com>
2018-06-10 18:55:59 -0400

    Update diyaltimeter.ino

--- diyaltimeter.ino
+++ diyaltimeter.ino
@@ -1,10 +1,11 @@
 
-#include <Wire.h>
-#include <SPI.h>
-#include <LiquidCrystal.h>
+#include <Wire.h> //Required for I2C
+#include <SPI.h> //For BMP dual sensor reading
+#include <LiquidCrystal.h> //LCD Library for 16x2
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
+//Constants for BMP Sensor
 #define BMP_SCK 13
 #define BMP_MISO 12
 #define BMP_MOSI 11 
@@ -15,7 +16,7 @@ Adafruit_BMP280 bme; // I2C
 //Adafruit_BMP280 bme(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
-
+//LCD Setup
 LiquidCrystal lcd(12,11,5,4,3,2);
 
 void setup()
@@ -34,22 +35,20 @@ void loop()
     lcd.clear();
 
     lcd.setCursor(0,0);
-    //lcd.print("Temp = ");
-    //lcd.print(bme.readTemperature());
-
+    
     //lcd.print("Pressure = ");
-    //lcd.print(bme.readPressure());
+    //lcd.print(bme.readPressure()); //Optional pressure reading
 
     
-    double Alt = bme.readAltitude(1020.8);
-    int AltFt = Alt * 3.28084;
+    double Alt = bme.readAltitude(1020.8);//Stores altitude reading in Alt
+    int AltFt = Alt * 3.28084;//Converts altitude in meters to feet
     lcd.print("Altitude:");
     lcd.print(AltFt);
     lcd.print("'");
 
 
-    double tempC = bme.readTemperature();
-    int tempF = tempC *1.8 + 32;
+    double tempC = bme.readTemperature(); //Stores sensor temperature reading in tempC
+    int tempF = tempC *1.8 + 32; //Convert Celsius reading to Farenheit
     lcd.setCursor(0,1);
     lcd.print("Temperature:");
     lcd.print(tempF);
